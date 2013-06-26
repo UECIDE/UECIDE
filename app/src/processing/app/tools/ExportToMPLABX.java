e991ac71427b71b2f1efb0ad9124ce3cc4ec0327
Matt Jenkins <matt@majenko.co.uk>
2013-06-27 00:02:58 +0100

    Cleaned up menus and started reworking plugin system

---
+++ app/src/processing/app/tools/ExportToMPLABX.java
@@ -0,0 +1,24 @@
+
+package processing.app.tools;
+
+import processing.app.*;
+import processing.core.PApplet;
+
+import java.io.*;
+
+public class ExportToMPLABX implements Tool {
+    Editor editor;
+
+    public void init(Editor editor) {
+        this.editor = editor;
+    }
+
+
+    public String getMenuTitle() {
+        return "Export to MPLAB-X";
+    }
+
+    public void run() {
+        System.err.println("Not Implemented");
+    }
+}
