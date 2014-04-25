d82c5d2076ef33fad15e58efbf33fc54732874ed
Matt Jenkins <matt@majenko.co.uk>
2014-04-25 21:32:22 +0100

    Automatically set UECIDE environment variable if unset
    External third party scripts and makefiles can now access the UECIDE
    environment variable to get the location of the "settings" directory.
    This is the directory which contains preferences.txt and also usually
    the compilers, boards, etc.  For instance, you could now use something
    like:
    
    $ $UECIDE/compilers/pic32-tools/bin/pic32-gcc ....
    
    within a script.
    
    This commit also standardizes the license boilerplate in all .java files
    in the main UECIDE core source.

--- core/src/uecide/app/Platform.java
+++ core/src/uecide/app/Platform.java
@@ -1,28 +1,36 @@
-/* -*- mode: java; c-basic-offset: 2; indent-tabs-mode: nil -*- */
-
 /*
-  Part of the Processing project - http://processing.org
-
-  Copyright (c) 2008 Ben Fry and Casey Reas
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software Foundation,
-  Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+ * Copyright (c) 2014, Majenko Technologies
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ * * Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ * 
+ * * Neither the name of Majenko Technologies nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 package uecide.app;
 
-import java.io.File;
+import java.io.*;
 
 import javax.swing.UIManager;
 
@@ -97,6 +105,62 @@ public class Platform {
   public File getDefaultSketchbookFolder() {
     return null;
   }
+
+    public void setSettingsFolderEnvironmentVariable() {
+        String ev = System.getenv("UECIDE");
+        File fn = Base.getSettingsFolder();
+        String fp = fn.getAbsolutePath();
+        if (ev == null || !(ev.equals(fp))) {
+            File bashrc = new File(System.getProperty("user.home"), ".bashrc");
+            File temprc = new File(System.getProperty("user.home"), ".bashrc.tmp");
+            if (!bashrc.exists()) {
+                bashrc = new File(System.getProperty("user.home"), ".profile");
+                temprc = new File(System.getProperty("user.home"), ".profile.tmp");
+            }
+            if (!bashrc.exists()) {
+                Debug.message("Unable to configure environment - neither .bashrc nor .profile seem to exist");
+                System.err.println("Unable to configure environment - neither .bashrc nor .profile seem to exist");
+                return;
+            }
+
+            try {
+            
+                boolean haveAdded = false;
+                BufferedReader in = new BufferedReader(new FileReader(bashrc));
+                BufferedWriter out = new BufferedWriter(new FileWriter(temprc));
+
+                String line;
+                while ((line = in.readLine()) != null) {
+                    if (line.startsWith("export UECIDE=")) {
+                        if (!haveAdded) {
+                            if (fp.contains(" ")) {
+                                out.write("export UECIDE=\"" + fp + "\"\n");
+                            } else {
+                                out.write("export UECIDE=" + fp + "\n");
+                            }
+                            haveAdded = true;
+                        }
+                    } else {
+                        out.write(line + "\n");
+                    }
+                }
+                if (!haveAdded) {
+                    if (fp.contains(" ")) {
+                        out.write("export UECIDE=\"" + fp + "\"\n");
+                    } else {
+                        out.write("export UECIDE=" + fp + "\n");
+                    }
+                    haveAdded = true;
+                }
+                in.close();
+                out.close();
+                temprc.renameTo(bashrc);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            
+        }
+    }
   
   
   public void openURL(String url) {
--- core/src/uecide/app/windows/Registry.java
+++ core/src/uecide/app/windows/Registry.java
@@ -1,3 +1,33 @@
+/*
+ * Copyright (c) 2014, Majenko Technologies
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ * * Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ * 
+ * * Neither the name of Majenko Technologies nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package uecide.app.windows;
 
 import java.io.UnsupportedEncodingException;
@@ -227,6 +257,35 @@ public class Registry {
 
         if(handle != 0) {
           if(advapi32.RegSetValueEx(handle, name, 0, WINNT.REG_SZ, data, data.length) == WINERROR.ERROR_SUCCESS) {
+            ret = true;
+          }
+          advapi32.RegCloseKey(handle);
+        }
+        return(ret);
+    } catch (Exception e) {
+        Base.error(e);
+        return false;
+    }
+  }
+
+  public static boolean setStringExpandValue(REGISTRY_ROOT_KEY rootKey, String subKeyName, String name, String value) {
+    try {
+        Advapi32 advapi32;
+        int handle;
+        byte[] data;
+        boolean ret = false;
+
+        // appears to be Java 1.6 syntax, removing [fry]
+        //data = Arrays.copyOf(value.getBytes("UTF-16LE"), value.length() * 2 + 2);
+        data = new byte[value.length() * 2 + 2];
+        byte[] src = value.getBytes("UTF-16LE");
+        System.arraycopy(src, 0, data, 0, src.length);
+
+        advapi32 = Advapi32.INSTANCE;
+        handle = openKey(rootKey, subKeyName, WINNT.KEY_READ | WINNT.KEY_WRITE);
+
+        if(handle != 0) {
+          if(advapi32.RegSetValueEx(handle, name, 0, WINNT.REG_EXPAND_SZ, data, data.length) == WINERROR.ERROR_SUCCESS) {
             ret = true;
           }
           advapi32.RegCloseKey(handle);
