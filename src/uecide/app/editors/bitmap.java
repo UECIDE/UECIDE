978eafe4d43d4a4d146727f83a89c61435490453
Matt Jenkins <matt@majenko.co.uk>
2014-06-05 11:16:56 +0100

    Added simple bitmap viewer

---
+++ src/uecide/app/editors/bitmap.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2014, Majenko Technologies
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ * * Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ * 
+ * * Neither the name of Majenko Technologies nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package uecide.app.editors;
+
+import uecide.app.*;
+import uecide.plugin.*;
+import uecide.app.debug.*;
+
+import java.awt.*;
+import java.awt.datatransfer.*;
+import java.awt.event.*;
+import java.awt.print.*;
+import java.awt.image.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.zip.*;
+
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.border.*;
+import javax.swing.text.*;
+import javax.swing.JToolBar;
+
+import javax.imageio.*;
+
+
+public class bitmap extends JPanel implements EditorBase {
+    File file = null;
+    boolean modified = false;
+    JScrollPane scrollPane;
+    Sketch sketch;
+    Editor editor;
+    ImageIcon image;
+    JLabel label;
+    BufferedImage bImage;
+
+    public bitmap(Sketch s, File f, Editor e) {
+        editor = e;
+        sketch = s;
+        this.setLayout(new BorderLayout());
+        scrollPane = new JScrollPane();
+        this.add(scrollPane, BorderLayout.CENTER);
+        loadFile(f);
+    }
+
+    public boolean loadFile(File f) {
+        try {
+            file = f;
+            bImage = ImageIO.read(f);
+            image = new ImageIcon(bImage);
+            label = new JLabel(image);
+            scrollPane.setViewportView(label);
+        } catch (Exception e) {
+            sketch.error(e);
+        }
+        return true;
+    }
+
+    public boolean isModified() {
+        return false;
+    }
+
+    public String getText() {
+        return "";
+    }
+    
+    public String getText(int s, int e) {
+        return "";
+    }
+    
+    public void setText(String text) {
+    }
+
+    public void setModified(boolean m) {
+    }
+
+    public void scrollTo(final int pos) {
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void revertFile() {
+        reloadFile();
+    }
+
+    public void populateMenu(JMenu menu, int flags) {
+    }
+
+    // Save the file contents to disk
+    public boolean save() {
+        return true;
+    }
+    
+    public void reloadFile() {
+        loadFile(file);
+    }
+
+    public void insertAtCursor(String text) {
+    }
+
+    public void insertAtStart(String text) {
+    }
+
+    public void insertAtEnd(String text) {
+    }
+}
