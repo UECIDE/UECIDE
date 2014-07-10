d9d4e9734680bcbb6c147d28ada92520b39c21cb
Matt Jenkins <matt@majenko.co.uk>
2014-07-11 00:42:17 +0100

    Fixed old plugins crashing new version

---
+++ src/uecide/plugin/BasePlugin.java
@@ -0,0 +1,26 @@
+package uecide.plugin;
+
+import uecide.app.*;
+import uecide.app.debug.*;
+import uecide.app.editors.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.*;
+import java.net.*;
+import java.util.zip.*;
+import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.event.*;
+import javax.swing.text.*;
+
+
+public class BasePlugin extends Plugin {
+    public void populateMenu(JMenu menu, int flags) {
+    }
+
+    public void addToolbarButtons(JToolBar toolbar, int flags) {
+    }
+
+}
+
