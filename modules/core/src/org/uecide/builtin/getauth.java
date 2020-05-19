a309b795c10203107dc93c59789cc64be35a0664
Matt Jenkins <matt@majenko.co.uk>
2020-05-19 21:28:57 +0100

    Migrated getauth action to Gui interface

--- modules/core/src/org/uecide/builtin/getauth.java
+++ modules/core/src/org/uecide/builtin/getauth.java
@@ -1,8 +1,7 @@
 package org.uecide.builtin;
 
 import org.uecide.*;
-import org.jline.reader.LineReader;
-import org.jline.reader.LineReaderBuilder;
+import org.uecide.gui.Gui;
 import javax.swing.*;
 import javax.swing.event.*;
 import java.awt.event.*;
@@ -24,74 +23,13 @@ public class getauth extends BuiltinCommand {
         String value = "";
         String exist = Preferences.get(dest);
 
-        if (UECIDE.isHeadless()) {
-            LineReader reader = LineReaderBuilder.builder().build();
-            System.out.print(prompt + ": ");
-            String in = reader.readLine();
-            if (in == "") {
-                value = exist;
-            } else {
-                value = null;
-            }
-            ctx.set(dest, value);
-            Preferences.set(dest, value);
-            return true;
-        }
+        Gui gui = ctx.getGui();
 
-
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-
-        JLabel label = new JLabel(prompt);
-        JPasswordField pass = new JPasswordField(exist);
-        panel.add(label);
-        panel.add(pass);
-        String[] options = new String[]{"OK", "Cancel"};
-
-
-        pass.addAncestorListener( new AncestorListener() {
-          @Override
-          public void ancestorRemoved( final AncestorEvent event ) {
-          }
-          @Override
-          public void ancestorMoved( final AncestorEvent event ) {
-          }
-          @Override
-          public void ancestorAdded( final AncestorEvent event ) {
-            // Ask for focus (we'll lose it again)
-            pass.requestFocusInWindow();
-          }
-        } );
-
-        pass.addFocusListener( new FocusListener() {
-          @Override
-          public void focusGained( final FocusEvent e ) {
-          }
-          @Override
-          public void focusLost( final FocusEvent e ) {
-            if( isFirstTime ) {
-              // When we lose focus, ask for it back but only once
-              pass.requestFocusInWindow();
-              isFirstTime = false;
-            }
-          }
-          private boolean isFirstTime = true;
-        } );
-   
-        int option = JOptionPane.showOptionDialog(null, panel, "Authentication",
-                                 JOptionPane.NO_OPTION, JOptionPane.PLAIN_MESSAGE,
-                                 null, options, options[0]);
-        if(option == 0) // pressing OK button
-        {
-            char[] password = pass.getPassword();
-            String p = new String(password);
-            ctx.set(dest, p);
-            Preferences.set(dest, p);
-            return true;
-        }
-
-        ctx.error("Authentication Cancelled");
-        return false;
+        value = gui.askPassword(prompt, exist);
+        if (value == null) return false;
+        ctx.set(dest, value);
+        Preferences.set(dest, value);
+        return true;
     }
 
     public void kill() {
